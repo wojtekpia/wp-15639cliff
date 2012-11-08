08bab993f29f28d4074171d876253a9502af07a0
unknown <WPiaseczny@WPIASECZ01VL.corp.homestore.net>
2012-11-07 16:51:31 -0800

    simple school template and 1 entry

---
+++ js/custom/code/data.js
@@ -0,0 +1,126 @@
+var school = {
+	"name" : "Earl Marriott Secondary",
+	"level": "high_school", 
+    "type": "public", 
+    "phone_number": "(604) 531-8354",
+	"address" : {
+		"line" : "15751 16 Ave",
+		"city" : "Surrey",
+		"state_code" : "BC",
+		"postal_code" : "V4A 1S1",
+		"lat" : 49.031340,
+		"long" : -122.784983
+	},
+	"rank" : {
+		"average" : "81/261",
+		"by" : {
+			"year" : {
+				"2010-11" : "126/280"
+			}
+		},
+	},
+	"rating" : {
+		"fraser_institute" : {
+			"href" : "http://britishcolumbia.compareschoolrankings.org/secondary/Earl_Marriott_Secondary/Surrey/Report_Card.aspx",
+			"score" : "6.2/10"
+		}
+	},
+	"general_info" : [
+		{"name":"Gr 12 Enrollment","value":"357"},
+		{"name":"ESL","value":"3.2%"},
+		{"name":"Special needs","value":"9.1%"},
+		{"name":"French Immersion","value":"24.6%"},
+		{"name":"Parents' average income","value":"$89,400"},
+		{"name":"Actual rating vs. predicted based on parents' avg. inc.","value":"-0.7"},
+	],
+	
+	"academic_performance" : [{
+		"name" : "Average exam mark",
+		"by" : {
+			"year" : {
+				"2007" : 68.4,
+				"2008" : 70.4,
+				"2009" : 70.4,
+				"2010" : 70.4,
+				"2011" : 69.7,
+			}
+		}
+	}, {
+		"name" : "Percentage of exams failed",
+		"by" : {
+			"year" : {
+				"2007" : 8.1,
+				"2008" : 6.8,
+				"2009" : 5.3,
+				"2010" : 6.3,
+				"2011" : 8.9,
+			}
+		}
+	}, {
+		"name" : "School vs exam mark difference",
+		"by" : {
+			"year" : {
+				"2007" : 5.0,
+				"2008" : 2.8,
+				"2009" : 2.0,
+				"2010" : 3.9,
+				"2011" : 4.2,
+			}
+		}
+	}, {
+		"name" : "English gender gap",
+		"by" : {
+			"year" : {
+				"2007" : "F 3.1",
+				"2008" : "F 7.2",
+				"2009" : "F 3.7",
+				"2010" : "F 4.1",
+				"2011" : "F 3.9",
+			}
+		}
+	}, {
+		"name" : "Math gender gap",
+		"by" : {
+			"year" : {
+				"2007" : "F 0.9",
+				"2008" : "F 1.0",
+				"2009" : "F 1.7",
+				"2010" : "F 0.3",
+				"2011" : "M 3.8",
+			}
+		}
+	}, {
+		"name" : "Graduation rate",
+		"by" : {
+			"year" : {
+				"2007" : 94.1,
+				"2008" : 96.6,
+				"2009" : 95.0,
+				"2010" : 93.9,
+				"2011" : 92.8,
+			}
+		}
+	}, {
+		"name" : "Delayed advancement rate",
+		"by" : {
+			"year" : {
+				"2007" : 26.1,
+				"2008" : 8.6,
+				"2009" : 11.8,
+				"2010" : 23.2,
+				"2011" : 21.1,
+			}
+		}
+	}, {
+		"name" : "Overall rating out of 10",
+		"by" : {
+			"year" : {
+				"2007" : 6.8,
+				"2008" : 7.3,
+				"2009" : 7.1,
+				"2010" : 6.4,
+				"2011" : 6.2,
+			}
+		}
+	}]
+}
