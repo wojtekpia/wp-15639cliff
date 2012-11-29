44469d1503c21a2dd6b93c7996c5f2684fbeb7f9
Wojtek P <wojtek_p@hotmail.com>
2012-11-28 20:17:27 -0800

    deleting old files

--- js/custom/code/map_helper.js
+++
@@ -1,9 +0,0 @@
-var map_helper = {
-    geoJsonPolygonToLatLngArray: function(geoJsonPolygon) {
-    	var coords = [];
-    	for(var i = 0; i < geoJsonPolygon.coordinates[0].length; i++) {
-    		coords.push(new google.maps.LatLng(geoJsonPolygon.coordinates[0][i][1],geoJsonPolygon.coordinates[0][i][0]));
-    	}
-		return coords;
-    }
-};
