89b2dc36f5f296498191659a2de6f3536fe06d9d
unknown <WPiaseczny@WPIASECZ01VL.corp.homestore.net>
2012-12-05 22:36:53 -0800

    made nav arrow better

--- js/custom/template/listing_header.js
+++ js/custom/template/listing_header.js
@@ -37,6 +37,7 @@ widgets.listing_header = {
 				</div>\
 			</div>\
 		</div>\
+				<div id=\"temp-value\"></div>\
 		<div class=\"progress-bar-wrapper page-width \">\
 			<ul>\
 				<li id=\"listing_overview_nav\"><a href=\"#listing_overview\" target=\"_self\">Overview</a></li>\
@@ -53,6 +54,7 @@ widgets.listing_header = {
 ",
 
 	render: function(selector,listing) {
+
 		var template = _.template(this.template);
 		var html = template({'listing' : listing});
 		$(selector).html(html);
@@ -66,6 +68,66 @@ widgets.listing_header = {
 	},
 	
     onScroll: function(eventData) {
+    	var navDivToBodyDivMapping = [
+    		{"nav":"listing_overview_nav","body":"listing_overview"},
+    		{"nav":"listing_features_nav","body":"listing_features"},
+    		{"nav":"schools_nav","body":"schools"},
+    		{"nav":"neighborhood_nav","body":"neighborhoods"},
+    		{"nav":"designer_nav","body":"designer"},
+    		{"nav":"builder_nav","body":"builder"}
+    	];
+
+    	//"listing_overview_nav","listing_features_nav","schools_nav","neighborhood_nav","designer_nav","builder_nav"];
+    	var currentPosition = $(window).scrollTop() + $("#listing_header").height();
+    	var tempval = "";
+    	var activeNavBodyMap = null;
+    	var verticalPercentIntoDiv = 0.0;
+    		
+    	for(var i = 0; i < navDivToBodyDivMapping.length; i++) {
+    		var bodyDiv = $("#" + navDivToBodyDivMapping[i].body);
+    		var bodyTop = bodyDiv.offset().top;
+    		var bodyBottom = bodyTop + bodyDiv.height();
+    		
+    		if(currentPosition > bodyTop && currentPosition <= bodyBottom) {
+    			activeNavBodyMap = navDivToBodyDivMapping[i];
+    			verticalPercentIntoDiv = (currentPosition - bodyTop) / (bodyBottom - bodyTop);
+    			
+    			tempval = navDivToBodyDivMapping[i].body;
+    			break;
+    		}
+    	}
+    	
+    	if(activeNavBodyMap != null) {
+    		var navDiv = $("#" + activeNavBodyMap.nav);
+    		var navLeft = navDiv.position().left;
+    		//var navRight = navLeft + navDiv.width;
+    		// TODO: include start offset.
+    		$("#nav_bar").css('left',(verticalPercentIntoDiv*(navDiv.width()) + navLeft) + 'px');
+    		//tempval = (verticalPercentIntoDiv*(navDiv.width()) + navLeft) + 'px';
+    	}
+    	//$('#temp-value').html(tempval);
+    	
+    	
+//    	if(currentPosition > $("#listing_overview").offset().top && currentPosition < $("#listing_overview").offset().top + $("#listing_overview").height()) {
+  //  		tempval = "listing_overview";
+    //	}
+    	
+/*    	tempval += $(window).scrollTop() + $("#listing_header").height();
+    	tempval += " ";
+    	tempval += $("#listing_header").offset().top + $("#listing_header").height();
+    	tempval += " ";
+    	tempval += $("#listing_overview").offset().top;
+    	tempval += " ";
+    	tempval += $("#listing_overview").offset().top + $("#listing_overview").height();
+  */  	
+    	
+    	// start arrow in middle of first item.
+    	// get position just below nav
+    	// find out what contains that position
+    	// move arrow proportionatly with that container
+    	
+    	// if the bottom of the nav thing is inside a div. pretend that the nav is moving on a frozen canvas.
+
     	// TODO: this needs to be based on a set of known anchors instead of purely linear.
     	// get width of nav bar
     	var navDivs = ["listing_overview_nav","listing_features_nav","schools_nav","neighborhood_nav","designer_nav","builder_nav"];
@@ -81,6 +143,6 @@ widgets.listing_header = {
     	var pageHeight = $(document).height();
     	var screenTop = $(document).scrollTop();
     	
-    	$("#nav_bar").css('left',(screenTop/pageHeight*(navWidthEnd-navWidthStart) + navWidthStart + $("#builder_nav").width()/2) + 'px');
+    	//$("#nav_bar").css('left',(screenTop/pageHeight*(navWidthEnd-navWidthStart) + navWidthStart + $("#builder_nav").width()/2) + 'px');
     }
 };
