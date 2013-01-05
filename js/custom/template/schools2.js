40f1b1207a61d916095380dffcc0f2b3ae552586
Wojtek P <wojtek_p@hotmail.com>
2013-01-04 19:04:33 -0800

    added school data, put schools into tabs and table

---
+++ js/custom/template/schools2.js
@@ -0,0 +1,54 @@
+widgets.schools2 = {
+	template:"\
+<h2 class=\"h2 inline\">Schools for <span class=\"address-string\"></span></h2>\
+	<div id=\"school-details\">\
+<div id=\"school-tabs\">\
+    <ul>\
+    	<% for(var s = 0; s < school_groups.length; s++){ %>\
+        	<li><a href=\"#<%= widgets.schools2.toTabName(school_groups[s].name) %>\"><%= school_groups[s].name %></a></li>\
+        <% } %>\
+    </ul>\
+	<% for(var s = 0; s < school_groups.length; s++){ %>\
+	    <div id=\"<%= widgets.schools2.toTabName(school_groups[s].name) %>\">\
+	        <table>\
+	        	<tr>\
+	        		<th class=\"sch-serves-col\">Serves Home</th>\
+	        		<th class=\"sch-name-col\">School Name</th>\
+	        		<th class=\"sch-type-col\">Type</th>\
+	        		<th class=\"sch-grade-col\">Grades</th>\
+	        		<th class=\"sch-rating-col\">Rating</th>\
+	        		<th class=\"sch-distance-col\">Distance</th>\
+	        	</tr>\
+	        	<% for(var i = 0; i < school_groups[s].schools.length; i++){\
+	        		var school = school_groups[s].schools[i] %>\
+	        		<tr>\
+	        			<td><%= school.assigned %></td>\
+	        			<td><%= school.name %></td>\
+	        			<td><%= school.type %></td>\
+	        			<td><%= school.level %></td>\
+	        			<td><%= school.rating.fraser_institute.score %></td>\
+	        			<td><%= geo.distance(school.address.lat, school.address.long, listing.address.lat, listing.address.long).toFixed(2) %> km</td>\
+	        		</tr>\
+	        	<% } %>\
+	        </table>\
+	    </div>\
+    <% } %>\
+</div>\
+	</div>\
+",
+
+	render: function(selector,school_groups) {
+		var template = _.template(this.template);
+		var html = template({'school_groups' : school_groups, 'listing' : listing});
+		$(selector).html(html);
+		
+		$(function() {
+	        $( "#school-tabs" ).tabs();
+	    });
+	},
+	
+	toTabName: function(name) {
+		return "sch-tab-" + name.toLowerCase().replace(/ /g,'-');
+	}
+};
+
